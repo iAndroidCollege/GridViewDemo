345cb3b1a5ed5936708a8a57e3bc3e56690f8bf8
iAndroidCollege <collegeandroid2015@gmail.com>
2015-12-11 10:42:01 +0200

    GridView Setup - Added xml layout for items. Added dimensions

--- app/src/main/java/college/edu/tomer/gridviewdemo/MainActivity.java
+++ app/src/main/java/college/edu/tomer/gridviewdemo/MainActivity.java
@@ -5,16 +5,28 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.photo_grid)
+    GridView photoGrid;
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
---
+++ app/src/main/java/college/edu/tomer/gridviewdemo/Photo.java
@@ -0,0 +1,13 @@
+package college.edu.tomer.gridviewdemo;
+
+public class Photo {
+
+    public Photo(String url, Float rating) {
+        this.url = url;
+        this.rating = rating;
+    }
+
+    private String url;
+    private Float rating;
+
+}
